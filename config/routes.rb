77e990072f55a90df0646752a788dfb2e23a2cc8
Angela Zhou <a03zhou@gmail.com>
2023-12-09 13:44:46 -0500

    add About Us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+  get '/about', to: 'about#show'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  resources :about, only: [:index]
 
   resource :cart, only: [:show] do
     post   :add_item
