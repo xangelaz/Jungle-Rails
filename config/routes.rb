a4a4ea91fc3606470cdd30cb4f8a8d90ba25a6dd
Angela Zhou <a03zhou@gmail.com>
2023-12-09 21:25:37 -0500

    update routes file with login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: 'products#index'
   get '/about', to: 'about#show'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
